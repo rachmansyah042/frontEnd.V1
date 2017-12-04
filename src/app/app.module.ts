f6f1b21cbabcd3fe64f6cbfbc5ce955d206838b9
rachmansyah042 <rachmansyah042@gmail.com>
2017-12-04 16:04:30 +0700

    validasi, form group

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
@@ -13,7 +15,11 @@ import { HeaderComponent } from './header/header.component';
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,41 @@ export class HeaderComponent implements OnInit {
 
   showLogin:boolean = false;
 
+  rForm: FormGroup;
+  post:any;
+  email:string ='';
+  password:string = '';
+  emailAlertMessage:string ='';
+  passwordAlertMessage:string ='';
+
+
   toggleLogin():void {
     this.showLogin = !this.showLogin;
   }
-  constructor() { }
+
+
+  
+  constructor(private fb: FormBuilder) {
+
+    this.initializeErrorMessage();
+
+    this.rForm = fb.group({
+      'password':[null, Validators.required],
+      'email':[null, Validators.required]
+
+    });
+   }
+
+   initializeErrorMessage() {
+    this.emailAlertMessage = "Email harus diisi";
+    this.passwordAlertMessage = "Password harus diisi";
+  }
+
+
+   addPost (post) {
+     this.email = post.email;
+     this.password = post.password;
+   }
 
   ngOnInit() {
   }
